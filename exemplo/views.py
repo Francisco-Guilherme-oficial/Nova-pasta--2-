55f1e360c9b1688b9630f9af2151eb44b5d33da9
Francisco Guilherme <frcog.monteiro@gmail.com>
2024-07-19 10:21:48 -0300

    Lista de usuario

--- exemplo/views.py
+++ exemplo/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from datetime import datetime
 
 def index(request):
-    return render(request, "index.html")
+    
+    lista_usuarios = [
+        {"nome": "Francisco", "Idade": 25},
+        {"nome": "Willian", "Idade": 80},
+        {"nome": "Afton", "Idade": 30},
+        {"nome": "Scott", "Idade": 25},
+        {"nome": "Cawton", "Idade": 396},
+    ]
+    context = {
+        "usuarios": lista_usuarios
+    }
+    return render(request, "index.html", context)
