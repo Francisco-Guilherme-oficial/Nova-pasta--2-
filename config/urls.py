84ea1aea9fd10f867f6f8f6de016fa3e5d402843
Francisco Guilherme <frcog.monteiro@gmail.com>
2024-07-19 09:08:46 -0300

    pagina de exemplo criada

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include("exemplos.urls")),
+    path('', include("exemplo.urls")),
     path('admin/', admin.site.urls),
 ]
